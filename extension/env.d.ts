544671237bff44e282d786288a28acf4736d5ff5
flowerslover151 <pangelinanpanagiotopoul2007@gmail.com>
2026-09-15 09:48:34 -0400

    Add files via upload

---
+++ extension/env.d.ts
@@ -0,0 +1,87 @@
+/// <reference types="styled-jsx" />
+
+declare module "webext-redux/lib/strategies/deepDiff/diff" {
+  // This should be : DiffStrategy, but importing webext-redux to reuse
+  // DiffStrategy results in an error augmenting the diff module.
+  export default function (): (oldObj: unknown, newObj: unknown) => unknown
+}
+
+declare module "webext-redux/lib/strategies/deepDiff/patch" {
+  // This should be : DiffStrategy, but importing webext-redux to reuse
+  // DiffStrategy results in an error augmenting the patch module.
+  export default function (): (oldObj: unknown, newObj: unknown) => unknown
+}
+
+// Although you would expect this file to be unnecessary, removing it will
+// result in a handful of type errors. See PR #196.
+
+declare module "styled-jsx/style"
+
+type BaseWalletProvider = {
+  providerInfo?: {
+    label: string
+    injectedNamespace: string
+    iconURL: string
+    identityFlag?: string
+    checkIdentity?: (provider: WalletProvider) => boolean
+  }
+  emit: (eventName: string | symbol, ...args: unknown[]) => boolean
+  on: (
+    eventName: string | symbol,
+    listener: (...args: unknown[]) => void,
+  ) => unknown
+  removeListener: (
+    eventName: string | symbol,
+    listener: (...args: unknown[]) => void,
+  ) => unknown
+}
+
+type WalletProvider = BaseWalletProvider & {
+  [optionalProps: string]: unknown
+}
+
+type TahoProvider = BaseWalletProvider & {
+  isTally: true
+  isTaho: true
+  tahoSetAsDefault: boolean
+  send: (method: string, params: unknown[]) => void | Promise<unknown>
+}
+
+type WindowEthereum = WalletProvider & {
+  isMetaMask?: boolean
+  tallySetAsDefault?: boolean
+  tahoSetAsDefault?: boolean
+  isTally?: boolean
+  isTaho?: boolean
+  autoRefreshOnNetworkChange?: boolean
+}
+interface Window {
+  walletRouter?: {
+    currentProvider: WalletProvider
+    providers: WalletProvider[]
+    shouldSetTallyForCurrentProvider: (
+      shouldSetTaho: boolean,
+      shouldReload?: boolean,
+    ) => void
+    shouldSetTahoForCurrentProvider: (
+      shouldSetTaho: boolean,
+      shouldReload?: boolean,
+    ) => void
+    routeToNewNonTahoDefault: (request: unknown) => Promise<unknown>
+    reemitTahoEvent: (event: string | symbol, ...args: unknown[]) => boolean
+    getProviderInfo: (
+      provider: WalletProvider,
+    ) => WalletProvider["providerInfo"]
+    addProvider: (newProvider: WalletProvider) => void
+  }
+  tally?: TahoProvider
+  taho?: TahoProvider
+  ethereum?: WindowEthereum
+  oldEthereum?: WindowEthereum
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "production" | "development" | "test"
+  }
+}
---
+++ wallet-core/base-assets.ts
@@ -0,0 +1,144 @@
+import { CoinGeckoAsset } from "../assets"
+import { NetworkBaseAsset } from "../networks"
+
+const ETH: NetworkBaseAsset = {
+  chainID: "1",
+  name: "Ether",
+  symbol: "ETH",
+  decimals: 18,
+  metadata: {
+    coinGeckoID: "ethereum",
+    tokenLists: [],
+  },
+}
+
+const ARBITRUM_ONE_ETH: NetworkBaseAsset = {
+  ...ETH,
+  chainID: "42161",
+  metadata: {
+    coinGeckoID: "ethereum",
+    tokenLists: [],
+  },
+}
+
+const ARBITRUM_NOVA_ETH: NetworkBaseAsset = {
+  ...ETH,
+  chainID: "42170",
+  metadata: {
+    coinGeckoID: "ethereum",
+    tokenLists: [],
+  },
+}
+
+const OPTIMISTIC_ETH: NetworkBaseAsset = {
+  ...ETH,
+  chainID: "10",
+  metadata: {
+    coinGeckoID: "ethereum",
+    tokenLists: [],
+  },
+}
+
+const SEPOLIA_ETH: NetworkBaseAsset = {
+  ...ETH,
+  chainID: "11155111",
+  metadata: {
+    coinGeckoID: "ethereum",
+    tokenLists: [],
+  },
+}
+
+const ARBITRUM_SEPOLIA_ETH: NetworkBaseAsset = {
+  ...ETH,
+  chainID: "421614",
+  metadata: {
+    coinGeckoID: "ethereum",
+    tokenLists: [],
+  },
+}
+
+const RBTC: NetworkBaseAsset = {
+  chainID: "30",
+  name: "RSK Token",
+  symbol: "RBTC",
+  decimals: 18,
+  metadata: {
+    coinGeckoID: "rootstock",
+    tokenLists: [],
+  },
+}
+
+const MATIC: NetworkBaseAsset = {
+  chainID: "137",
+  name: "Matic Token",
+  symbol: "MATIC",
+  decimals: 18,
+  metadata: {
+    coinGeckoID: "matic-network",
+    tokenLists: [],
+    websiteURL: "https://polygon.technology/",
+  },
+}
+
+const AVAX: NetworkBaseAsset = {
+  chainID: "43114",
+  name: "Avalanche",
+  symbol: "AVAX",
+  decimals: 18,
+  metadata: {
+    coinGeckoID: "avalanche-2",
+    tokenLists: [],
+    websiteURL: "https://avax.network/",
+  },
+}
+
+const BNB: NetworkBaseAsset = {
+  chainID: "56",
+  name: "Binance Coin",
+  symbol: "BNB",
+  decimals: 18,
+  metadata: {
+    coinGeckoID: "binancecoin",
+    tokenLists: [],
+    websiteURL: "https://bnbchain.org",
+  },
+}
+
+const ZK_SYNC_ETH: NetworkBaseAsset = {
+  ...ETH,
+  chainID: "324",
+}
+
+const MEZO_BTC: NetworkBaseAsset & Required<CoinGeckoAsset> = {
+  chainID: "31611",
+  name: "Bitcoin",
+  symbol: "BTC",
+  decimals: 18,
+  metadata: {
+    coinGeckoID: "bitcoin",
+    tokenLists: [],
+  },
+}
+
+const MEZO_MAINNET_BTC: NetworkBaseAsset & Required<CoinGeckoAsset> = {
+  ...MEZO_BTC,
+  chainID: "31612",
+}
+
+export const BASE_ASSETS_BY_CUSTOM_NAME = {
+  ETH,
+  MATIC,
+  MEZO_BTC,
+  MEZO_MAINNET_BTC,
+  RBTC,
+  AVAX,
+  BNB,
+  ARBITRUM_ONE_ETH,
+  ARBITRUM_NOVA_ETH,
+  OPTIMISTIC_ETH,
+  SEPOLIA_ETH,
+  ARBITRUM_SEPOLIA_ETH,
+  ZK_SYNC_ETH,
+}
+
+export const BASE_ASSETS = Object.values(BASE_ASSETS_BY_CUSTOM_NAME)
